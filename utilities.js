fb494698a79da4391cf9c3d591f32ff4cbbf99f4
Jennifer Winer <jeniwiner@gmail.com>
2018-10-04 21:53:32 -0600

    remove semi colons to match style

--- utilities.js
+++ utilities.js
@@ -1,7 +1,7 @@
 const findArticleFlag = args => {
   let flag;
   if (args[1] && args[1].charAt(0) === '-') flag = args[1].slice(1)
-  else if (args[2] && args[2].charAt(0) === '-') flag = args[2].slice(1);
+  else if (args[2] && args[2].charAt(0) === '-') flag = args[2].slice(1)
   // make sure arg is a number and limit to max 10 articles
   let numberOfArticles = new Number(flag);
   if (numberOfArticles == 0 || isNaN(numberOfArticles) || numberOfArticles > 10) numberOfArticles = 10
