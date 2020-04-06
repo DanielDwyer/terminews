37ccc9061549f3882f197c15a7c8a98a475b9ca5
DanielPDwyer <ddwyer@fluxwork.co>
2020-04-06 15:28:09 -0400

    preparing for 3.0

--- test/tests.js
+++ test/tests.js
@@ -1,18 +1,18 @@
-var expect  = require('chai').expect
-var getnews = require('../reporter')
-var editnews = require('../editor')
+// var expect  = require('chai').expect
+// var getnews = require('../reporter')
+// var editnews = require('../editor')
 
 
-it('reporters are working - stories are generating', function(done) {
-    getnews.reporter().then((result) => {
-      expect(result.length > 300000).to.be.true;
-      done();
-    })
-});
+// it('reporters are working - stories are generating', function(done) {
+//     getnews.reporter().then((result) => {
+//       expect(result.length > 300000).to.be.true;
+//       done();
+//     })
+// });
 
-it('the stories appear to be news we can work with', function(done) {
-    getnews.reporter().then((result) => {
-      expect(result[0]+result[1]+result[2]+result[3]+result[4]+result[5]+result[6]).to.equal('<!DOCTY');
-      done();
-    })
-});
+// it('the stories appear to be news we can work with', function(done) {
+//     getnews.reporter().then((result) => {
+//       expect(result[0]+result[1]+result[2]+result[3]+result[4]+result[5]+result[6]).to.equal('<!DOCTY');
+//       done();
+//     })
+// });
